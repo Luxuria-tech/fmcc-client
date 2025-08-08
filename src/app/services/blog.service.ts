da58b71a2113d0f891cfb0ce15f46215c9ff180b
lina237 <143194626+lina237@users.noreply.github.com>
2025-08-08 12:24:28 -0700

    Update blog.service.ts

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod.ts';
+import { environment } from 'src/environments/environment';
+
 
 export interface Blog {
   id?: number;
