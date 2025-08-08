07bb6655293db4786512e50b54e7c214aff408bd
lina237 <143194626+lina237@users.noreply.github.com>
2025-08-08 11:10:12 -0700

    Update event.service.ts

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {environment} from 'src/environments/environment';
+
 
 export interface Event {
   id?: number;
