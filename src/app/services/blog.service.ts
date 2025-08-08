88d4e31a794d0fe54657f4cda26389066e2e9f71
lina237 <143194626+lina237@users.noreply.github.com>
2025-08-08 12:42:07 -0700

    Update blog.service.ts

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-
 export interface Blog {
   id?: number;
   title: string;
@@ -14,49 +13,46 @@ export interface Blog {
   image_url?: string;
 }
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class BlogService {
-  private api = `${environment.apiUrl}/blogs`;
+  private readonly apiUrl = `${environment.apiUrl}/blogs`;
 
   constructor(private http: HttpClient) {}
 
   list(): Observable<Blog[]> {
-    return this.http.get<Blog[]>(this.api);
+    return this.http.get<Blog[]>(this.apiUrl);
   }
 
   getBlogById(id: string | number): Observable<Blog> {
-    return this.http.get<Blog>(`${this.api}/${id}`);
+    return this.http.get<Blog>(`${this.apiUrl}/${id}`);
   }
 
   add(blog: Blog): Observable<Blog> {
-    const fd = new FormData();
-    Object.entries(blog).forEach(([k, v]) => {
-      if (v !== undefined && v !== null) {
-        if (v instanceof File) {
-          fd.append(k, v, v.name);
-        } else {
-          fd.append(k, String(v));
-        }
-      }
-    });
-    return this.http.post<Blog>(this.api, fd);
+    return this.http.post<Blog>(this.apiUrl, this.prepareFormData(blog));
   }
 
   update(id: number, blog: Blog): Observable<Blog> {
-    const fd = new FormData();
-    Object.entries(blog).forEach(([k, v]) => {
-      if (v !== undefined && v !== null) {
-        if (v instanceof File) {
-          fd.append(k, v, v.name);
-        } else {
-          fd.append(k, String(v));
-        }
-      }
-    });
-    return this.http.put<Blog>(`${this.api}/${id}`, fd);
+    return this.http.put<Blog>(`${this.apiUrl}/${id}`, this.prepareFormData(blog));
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.api}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  /** Utility to prepare form data for add/update */
+  private prepareFormData(blog: Blog): FormData {
+    const fd = new FormData();
+    Object.entries(blog).forEach(([key, value]) => {
+      if (value !== undefined && value !== null) {
+        if (value instanceof File) {
+          fd.append(key, value, value.name);
+        } else {
+          fd.append(key, String(value));
+        }
+      }
+    });
+    return fd;
   }
 }
